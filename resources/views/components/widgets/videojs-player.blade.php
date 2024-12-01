d141126950588acf3862d99422aac193575a4af4
Myo Myint Aung ( Juno ) <myomyintaung512@gmail.com>
2024-12-01 22:27:12 +0630

    Auto Play Update

--- resources/views/components/widgets/videojs-player.blade.php
+++ resources/views/components/widgets/videojs-player.blade.php
@@ -1,15 +1,19 @@
 @props(['match'])
 <div class="video-js-responsive-container vjs-hd">
-    <video id="video" class="video-js vjs-default-skin" preload="none" crossorigin="true" controls width="640"
+    <video id="video" autoplay="true" muted preload="auto"  class="video-js vjs-default-skin" preload="none" crossorigin="true" controls width="640"
         height="268" controls>
+        <source src='{{ $match->live_embed }}' type='application/x-mpegURL'>
     </video>
 </div>
 
 
 <script type="text/javascript">
     jQuery(document).ready(function () {        
-        jQuery("#video").html("<source src='{{ $match->live_embed }}' type='application/x-mpegURL'>");
-        var ply = videojs("video");
+        var ply = videojs("video", {
+            autoplay: true,
+            muted: true,
+            controls: true
+        });
         ply.play();        
     });
 </script>
